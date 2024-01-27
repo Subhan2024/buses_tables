5df957f4f30d9c2fc997705bca0a5a2c03bc3a61
asp <asp@aptech.com>
2024-01-27 18:52:10 +0500

    interesting

--- tables_buses.sql
+++ tables_buses.sql
@@ -44,3 +44,34 @@ alter table swipes add constraint FK__swipes__card_id__4D94879B foreign key(card
 alter table cards alter column card_name text not null;
 
 select * into rider2 from riders;
+
+ALTER TABLE books RENAME COLUMN isbn to id_no;
+
+
+select * from reader;
+
+sp_columns reader;
+sp_rename 'rider2','reader';
+sp_rename 'reader.name' , 'reader_name' , 'column';
+
+create table collections(
+id integer primary key identity(1,2),
+title varchar(200)not null,
+accession_number float not null unique,
+acquired date
+);
+drop table collections;
+select * from collections;
+
+
+
+insert into collections(title , accession_number , acquired) values  ('profusion of flowers' , 56.257 , '1956-04-12'),
+('Farmers Working at Dawn' , 11.6152 , '1911-08-03'),
+('Spring Outig' , 14.76 , '1914-01-08'),
+('imaginative landscape' , 56.496 , NULL),
+('peonies and butterfly' , 06.1899 , '1906-01-01'),
+('Tile unitte' , 06.2437 , '1906-11-08'),
+('Statutee of a shrew' , 01.105 , '1901-02-11');
+
+delete from collections where acquired <= '1907-01-01';
+
