350b02cdcd137ebd89477792a3b505d8fba1efe3
asp <asp@aptech.com>
2024-02-01 19:02:00 +0500

    transaction_table

--- art.sql
+++ art.sql
@@ -31,7 +31,7 @@ foreign key(artist_id) references artist(id) on delete cascade,
 foreign key(collection_id) references collections(id)  on delete cascade
 );
 select * from artist;
-insert into artist(name) values ('li yin'),
+insert into artist(name) values ('Li Yin'),
 ('qian weicheng'),
 ('undefined artist'),
 ('zhou chen');
@@ -40,7 +40,7 @@ insert into created(artist_id , collection_id) values
 (1,1),
 (2,3),
 (4,5),
-(5,7);
+(6,7);
 drop table created;
 
 select * from created;
@@ -49,5 +49,30 @@ select * from collections;
 
 delete from artist where name = 'undefined artist';
 
-update created set artist_id = (select id from artist where name = 'zhou chen')
-where collection_id = (select id from artist where name = 'li yin');
+
+update created set artist_id =(select id from artist where name = 'zhou chen')
+where collection_id = (select id from collections where title = 'profusion of flowers');
+
+
+create table transactions(
+id integer primary key identity(1,1),
+title varchar(200),
+action varchar(200)
+);
+
+create trigger sell
+on collections
+after delete 
+as
+begin
+	insert into transactions(title , action)
+	select title, 'sold'
+	from deleted;
+ end;
+
+ select * from transactions;
+ select * from collections;
+
+ delete from collections where title = 'imaginative landscape';
+
+ select * trigger.table
